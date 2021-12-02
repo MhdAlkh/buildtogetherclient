b683dcb4a50089e6c6a3d4ac162761a1b0e6cf73
MhdAlkh <mhd.khalilalkhalil@gmail.com>
2021-12-02 16:31:18 +0100

    fix

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/buildtogether-frontend'),
+      dir: require('path').join(__dirname, './coverage/buildtogetherclient'),
       subdir: '.',
       reporters: [
         { type: 'html' },
